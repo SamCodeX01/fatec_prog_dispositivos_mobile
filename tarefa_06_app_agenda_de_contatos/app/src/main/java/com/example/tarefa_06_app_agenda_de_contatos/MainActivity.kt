6d433c2bdadfec2d7b59349fc3ed2219eeeb88c9
SamCodeX01 <txtstring@gmail.com>
2025-11-10 12:04:37 -0300

    tarefa_06_app_agenda_de_contatos

---
+++ tarefa_06_app_agenda_de_contatos/app/src/main/java/com/example/tarefa_06_app_agenda_de_contatos/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.tarefa_06_app_agenda_de_contatos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tarefa_06_app_agenda_de_contatos.ui.theme.Tarefa_06_app_agenda_de_contatosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tarefa_06_app_agenda_de_contatosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Tarefa_06_app_agenda_de_contatosTheme {
+        Greeting("Android")
+    }
+}
